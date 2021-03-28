0ecca1f5769d0fbf0c8d2a6c77f653345e6daacf
Druciak <ola.druciak@gmail.com>
2021-03-28 14:48:00 +0200

    Update of words and finish load creator

--- load_creator_for_searchers.py
+++ load_creator_for_searchers.py
@@ -1,49 +1,43 @@
+import requests
 import asyncio
-from timeit import default_timer
 from concurrent.futures import ThreadPoolExecutor
 
-async def get_data_asynchronous():
-    csvs_to_fetch = [
-        "ford_escort.csv",
-        "cities.csv",
-        "hw_25000.csv",
-        "mlb_teams_2012.csv",
-        "nile.csv",
-        "homes.csv",
-        "hooke.csv",
-        "lead_shot.csv",
-        "news_decline.csv",
-        "snakes_count_10000.csv",
-        "trees.csv",
-        "zillow.csv"
-    ]
-    print("{0:<30} {1:>20}".format("File", "Completed at"))
-    
-    # Note: max_workers is set to 10 simply for this example,
-    # you'll have to tweak with this number for your own projects
-    # as you see fit
-    with ThreadPoolExecutor(max_workers=10) as executor:
+def search(session, request):
+    with session.get(request) as response:
+        data = response.text
+        if response.status_code != 200:
+            print("FAILURE::{0}".format(url))
+
+        return data
+
+async def make_req_async(reqs):
+    with ThreadPoolExecutor(max_workers=len(reqs)) as executor:
         with requests.Session() as session:
             # Set any session parameters here before calling `fetch`
-
-            # Initialize the event loop        
             loop = asyncio.get_event_loop()
-            
-            # Set the START_TIME for the `fetch` function
-            START_TIME = default_timer()
-            
-            # Use list comprehension to create a list of
-            # tasks to complete. The executor will run the `fetch`
-            # function for each csv in the csvs_to_fetch list
             tasks = [
                 loop.run_in_executor(
                     executor,
-                    fetch,
-                    *(session, csv) # Allows us to pass in multiple arguments to `fetch`
+                    search,
+                    *(session, req) # Allows us to pass in multiple arguments to `fetch`
                 )
-                for csv in csvs_to_fetch
+                for req in reqs
             ]
-            
-            # Initializes the tasks to run and awaits their results
             for response in await asyncio.gather(*tasks):
                 pass
+
+def make_request(word):
+    return f"http://localhost:8983/solr/MemCore/select?q=content%3A*{word}*";
+
+def get_req_to_search():
+    with open('random_words.txt', 'r') as reader:
+        all_words = reader.readlines()
+        return [make_request(word) for word in all_words]
+
+def main():
+    loop = asyncio.get_event_loop()
+    requests = get_req_to_search()
+    future = asyncio.ensure_future(make_req_async(requests))
+    loop.run_until_complete(future)
+    print('--------DONE--------')
+main()
